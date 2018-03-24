b6bae1808ded0dfa2b89506e3e5ece3b1e72a2db
Kenny Chung <chunghlken@gmail.com>
2018-03-23 21:44:58 -1000

    #3 solution

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+more.addEventListener('click', moreContent);
+
+function moreContent(){
+  more.innerHTML = bacon;
+}
+
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
@@ -13,8 +19,17 @@ var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami ta
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
 
+function lessContent(){
+  less.style.display = 'none';
+}
+
+
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
+
+function zoom(){
+  biggie.style.fontSize = '150%';
+}
 
 
 //4. McDonalds
