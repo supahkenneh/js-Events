51d6086c308179ddf740f8e8871618cca4cb161c
Kenny Chung <chunghlken@gmail.com>
2018-03-23 22:02:23 -1000

    #5 solution

--- js/app.js
+++ js/app.js
@@ -48,6 +48,10 @@ function valueMenu(){
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
 
+function redFace(){
+  drink.style.color = 'red';
+  drink.style.fontSize = '30px';
+}
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
